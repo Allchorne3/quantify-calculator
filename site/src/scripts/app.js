7cd15b9ccfa67e95348f9d68379bc5f91ca0461a
Stefan Allchorne <allchorne39@sky.com>
2023-08-19 10:43:15 +0100

    Fix header styles

--- site/src/scripts/app.js
+++ site/src/scripts/app.js
@@ -2,8 +2,16 @@
 import tabs from './tabs'
 
 document.addEventListener('DOMContentLoaded', () => {
+    document.addEventListener('scroll', () => {
+        document.documentElement.classList.toggle('is-scrolled', window.scrollY > 30)
+    });
+
+    window.dispatchEvent(new Event('scroll'));
+
     if (document.documentElement.id === 'page-calculator') {
         // calculator.init();
     }
+
+    // Tab functionality
     tabs.runTabs();
 });
