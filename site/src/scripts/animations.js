b9bdc3b885023d1676d7dcec1d60cda38ef7e03c
Stefan Allchorne <allchorne39@sky.com>
2023-08-26 21:39:03 +0100

    Style updates

--- site/src/scripts/animations.js
+++ site/src/scripts/animations.js
@@ -10,23 +10,23 @@ const init = () => {
     })
 
     // line by line title animation. Home h1 only
-    let myText = new SplitType('#page-index .title.animated');
-    tl.from(myText.lines, {
+    let heading = new SplitType('#page-index .title.animated');
+    tl.from(heading.lines, {
         y: "100%",
         stagger: 0.1,
         duration: 0.4
     })
     tl.to('#page-index .title.animated', {
-        // opacity: 1,
+        opacity: 1,
         y: 0,
     })
     tl.to('#page-index .title.animated ~ p', {
         opacity: 1,
-        duration: .5 
+        duration: .5 ,
+        delay: -.4
     })
     tl.to('#page-index .hero-body a.button', {
         opacity: 1,
-        top: 0,
         duration: .3,
         delay: -.4
     })
