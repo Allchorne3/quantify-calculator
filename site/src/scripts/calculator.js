43bb6fa1248aa41405e335533aa699e6584016fb
Stefan Allchorne <allchorne39@sky.com>
2023-11-20 11:15:00 +0000

    fix error

--- site/src/scripts/calculator.js
+++ site/src/scripts/calculator.js
@@ -143,26 +143,26 @@ const init = () => {
 			limit: 5001,
 			percentage: document.querySelector('select option[value="1"]').dataset.percentage
 		},
-		2: {
-			limit: 20001,
-			percentage: document.querySelector('select option[value="2"]').dataset.percentage
-		},
-		3: {
-			limit: 50001,
-			percentage: document.querySelector('select option[value="3"]').dataset.percentage
-		},
-		4: {
-			limit: 200001,
-			percentage: document.querySelector('select option[value="4"]').dataset.percentage
-		},
-		5: {
-			limit: 500001,
-			percentage: document.querySelector('select option[value="5"]').dataset.percentage
-		},
-		6: {
-			limit: Infinity,
-			percentage: document.querySelector('select option[value="6"]').dataset.percentage
-		},
+		// 2: {
+		// 	limit: 20001,
+		// 	percentage: document.querySelector('select option[value="2"]').dataset.percentage
+		// },
+		// 3: {
+		// 	limit: 50001,
+		// 	percentage: document.querySelector('select option[value="3"]').dataset.percentage
+		// },
+		// 4: {
+		// 	limit: 200001,
+		// 	percentage: document.querySelector('select option[value="4"]').dataset.percentage
+		// },
+		// 5: {
+		// 	limit: 500001,
+		// 	percentage: document.querySelector('select option[value="5"]').dataset.percentage
+		// },
+		// 6: {
+		// 	limit: Infinity,
+		// 	percentage: document.querySelector('select option[value="6"]').dataset.percentage
+		// },
 	};
 
 	const calculateEarnings = (level, balance) => {
